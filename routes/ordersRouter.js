35d1c769c4c8eef3aafdd2139161c4119b05ac2b
Alina Cherevko <alinapavluhina@ukr.net>
2024-05-25 18:50:28 +0300

    Done

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
-import validateBody from "../helpers/validateBody";
 import { ordersSchema } from "../joiSchemas/joiOrdersSchemas";
 import { postOrder } from "../controllers/ordersControllers";
 import { isValidId } from "../middlewares/isValidId.js";
+import validateBody from "../helpers/validateBody.js";
 
 const ordersRouter = express.Router();
 
